3c59ac2a81da20d70c01000fc91e284776da1c74
Pete Shearer <pete.shearer@emblemseven.com>
2016-05-26 19:07:48 -0400

    Added Lesson 011 - Intro to Extensions

---
+++ 011-Intro to Extensions/Lesson 011 - Extensions.playground/Contents.swift
@@ -0,0 +1,51 @@
+// We can extend our own classes
+class Post {
+    var title: String = ""
+    var author: String = ""
+}
+
+var lesson001 = Post()
+lesson001.title = "Lesson 001 - Variables"
+lesson001.author = "Pete"
+
+extension Post {
+    var heading: String {
+        return "\(title) by \(author)"
+    }
+}
+
+print(lesson001.heading)
+
+
+// We can extend system classes
+
+extension String {
+    func DoubleMeUp() -> String {
+        return "\(self)\(self)"
+    }
+}
+
+print("Swift".DoubleMeUp()) // <-- Prints SwiftSwift
+
+// We can extend it again 
+
+extension String {
+    var squareOfLength: Int {
+        return self.characters.count*self.characters.count
+    }
+    
+    var firstLetter: String {
+        if let returnValue = self.characters.first {
+            return String(returnValue)
+        }
+        else {
+            return ""
+        }
+    }
+}
+
+print("Swift".squareOfLength) // <-- Prints 25
+print("Swift".firstLetter) // <-- Prints S
+
+
+
